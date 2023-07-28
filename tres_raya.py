08c7ab101edec5bfac00e8e0fcb6c73bd83f9ede
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2023-07-28 21:21:20 +0200

    funciones tablero y ganador implementadas

--- tres_raya.py
+++ tres_raya.py
@@ -4,3 +4,23 @@ Creación del famoso juego de mesa
 
 """
 
+# Función que imprimirá el tablero
+def imprimir_tablero(tablero):
+    for fila in tablero:
+        print(" | ".join(fila))
+        print("-" * 9)
+
+# Función que verifica el ganador de la partida
+def verificar_ganador(tablero, jugador):
+    for fila in tablero:
+        if all(elem == jugador for elem in fila):
+            return True
+
+    for columna in range(3):
+        if all(tablero[i][columna] == jugador for i in range(3)):
+            return True
+
+    if all(tablero[i][i] == jugador for i in range(3)) or all(tablero[i][2 - i] == jugador for i in range(3)):
+        return True
+
+    return False
