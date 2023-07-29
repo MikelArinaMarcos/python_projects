8b56858098055b0c3ca9357d9b7f7d7ab09ed47c
Mikel Arina Marcos <mikel.arina.marcos@gmail.com>
2023-07-29 22:04:53 +0200

    Juego completo con menú, niveles, muros, obstáculos, diferente comida y puntuación con récords

--- snake.py
+++ snake.py
@@ -16,6 +16,9 @@ pygame.display.set_caption("Snake Game")
 WHITE = (255, 255, 255)
 GREEN = (0, 128, 0)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+ORANGE = (255, 165, 0)
+GRAY = (128, 128, 128)
 
 # Direccionamientos
 UP = (0, -1)
@@ -23,28 +26,61 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Función para mostrar mensaje y esperar antes de reiniciar el juego
-def show_message(message):
-    font = pygame.font.Font(None, 36)
+# Función para mostrar mensaje y esperar antes de continuar
+def show_message(message, font_size=36):
+    font = pygame.font.Font(None, font_size)
     text = font.render(message, True, WHITE)
     SCREEN.blit(text, (WIDTH // 2 - text.get_width() // 2, HEIGHT // 2 - text.get_height() // 2))
     pygame.display.flip()
     time.sleep(2)
 
-def main():
-    # Inicialización de variables
+# Función para dibujar el texto en la pantalla
+def draw_text(text, font_size, x, y):
+    font = pygame.font.Font(None, font_size)
+    text_surface = font.render(text, True, WHITE)
+    SCREEN.blit(text_surface, (x, y))
+
+# Función para mostrar el menú de inicio
+def show_menu():
+    SCREEN.fill(GREEN)
+    draw_text("¡Bienvenido a Snake!", 48, WIDTH // 2 - 200, HEIGHT // 2 - 100)
+    draw_text("Presiona ENTER para comenzar", 24, WIDTH // 2 - 160, HEIGHT // 2 + 50)
+    pygame.display.flip()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    return
+
+# Función para comprobar colisiones
+def check_collision(snake):
+    head_x, head_y = snake[-1]
+    return head_x < 0 or head_x >= GRID_WIDTH or head_y < 0 or head_y >= GRID_HEIGHT or (head_x, head_y) in snake[:-1]
+
+# Función para jugar un nivel
+def play_level(level):
     snake = [(GRID_WIDTH // 2, GRID_HEIGHT // 2)]
     snake_direction = RIGHT
-    snake_speed = 10
-    food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
-    level = 1
-    food_to_next_level = 5
+    snake_speed = 10 + level * 2
+    snake_growth = 0
+    food_type = random.choice([RED, BLUE, ORANGE, WHITE])
+    if food_type == WHITE:
+        food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+    else:
+        food = (random.randint(2, GRID_WIDTH - 3), random.randint(2, GRID_HEIGHT - 3))
+    food_to_next_level = 5 + level * 5
     foods_collected = 0
-    game_over = False
+    score = 0
+
+    obstacles = [(2, 5), (5, 2), (GRID_WIDTH - 3, GRID_HEIGHT - 6), (GRID_WIDTH - 6, GRID_HEIGHT - 3)]
 
     clock = pygame.time.Clock()
 
-    while not game_over:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -62,42 +98,62 @@ def main():
         # Actualizar posición de la serpiente
         head_x, head_y = snake[-1]
         dx, dy = snake_direction
-        new_head = ((head_x + dx) % GRID_WIDTH, (head_y + dy) % GRID_HEIGHT)
+        new_head = (head_x + dx, head_y + dy)
 
-        if new_head in snake[:-1]:
-            show_message("¡Perdiste! Inténtalo de nuevo.")
-            snake.clear()
-            snake.append((GRID_WIDTH // 2, GRID_HEIGHT // 2))
-            snake_direction = RIGHT
-            snake_speed = 10
-            level = 1
-            food_to_next_level = 5
-            foods_collected = 0
+        if check_collision(snake) or new_head in obstacles:
+            return score
         else:
             snake.append(new_head)
             if new_head == food:
                 foods_collected += 1
+                if food_type == WHITE:
+                    score += 50
+                elif food_type == ORANGE:
+                    score += 30
+                elif food_type == BLUE:
+                    score += 20
+                else:
+                    score += 10 * level
+                snake_growth += 1
+
                 if foods_collected == food_to_next_level:
-                    level += 1
-                    snake_speed += 2
-                    food_to_next_level += 5
-                food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+                    return score
+                food_type = random.choice([RED, BLUE, ORANGE, WHITE])
+                if food_type == WHITE:
+                    food = (random.randint(0, GRID_WIDTH - 1), random.randint(0, GRID_HEIGHT - 1))
+                else:
+                    food = (random.randint(2, GRID_WIDTH - 3), random.randint(2, GRID_HEIGHT - 3))
             else:
-                snake.pop(0)
-        
+                if snake_growth > 0:
+                    snake_growth -= 1
+                else:
+                    snake.pop(0)
+
         # Dibujar el juego
         SCREEN.fill(GREEN)
         for segment in snake:
             pygame.draw.rect(SCREEN, WHITE, (segment[0] * GRID_SIZE, segment[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
-        pygame.draw.rect(SCREEN, RED, (food[0] * GRID_SIZE, food[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+        for obstacle in obstacles:
+            pygame.draw.rect(SCREEN, GRAY, (obstacle[0] * GRID_SIZE, obstacle[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
+        pygame.draw.rect(SCREEN, food_type, (food[0] * GRID_SIZE, food[1] * GRID_SIZE, GRID_SIZE, GRID_SIZE))
 
-        # Mostrar el nivel actual en pantalla
-        font = pygame.font.Font(None, 24)
-        text = font.render(f"Nivel: {level}", True, WHITE)
-        SCREEN.blit(text, (10, 10))
-
+        draw_text(f"Nivel: {level}", 24, 10, 10)
+        draw_text(f"Puntuación: {score}", 24, 10, 40)
         pygame.display.flip()
         clock.tick(snake_speed)
+
+def main():
+    show_menu()
+    high_score = 0
+    level = 1
+
+    while True:
+        score = play_level(level)
+        if score > high_score:
+            high_score = score
+
+        show_message(f"¡Perdiste! Puntuación: {score} | Récord: {high_score}", font_size=30)
+        level += 1
 
 if __name__ == "__main__":
     main()
